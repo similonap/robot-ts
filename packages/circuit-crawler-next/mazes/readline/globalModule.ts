dc1b808489f44a7a43564497b1bb9832f778f936
Andie Similon <andie.similon@ap.be>
2026-01-31 09:45:33 +0100

    fix readline

--- packages/circuit-crawler-next/mazes/readline/globalModule.ts
+++ packages/circuit-crawler-next/mazes/readline/globalModule.ts
@@ -4,8 +4,15 @@ const robot = game.getRobot("robot");
 
 if (robot) {
     game.addEventListener("robot_created", (robot) => {
-        if (robot.position.x === 7 && robot.position.y === 7) {
-
+        if (robot.name !== "MyRobot") {
+            game.fail("Robot name is not MyRobot");
         }
+        if (robot.position.x !== 7 || robot.position.y !== 7) {
+            game.fail("Robot is not at position 7, 7");
+        }
+        if (robot.color !== "#ff0000") {
+            game.fail("Robot is not red");
+        }
+        game.win("You created the correct robot");
     });
 }
